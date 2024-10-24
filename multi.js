45a013d0d262e7167acb16818d8d141e10448499
Quvonchbek <https://000quvonchbek.github.io/website/>
2024-10-24 23:26:43 +0500

    A

--- multi.js
+++ multi.js
@@ -10,28 +10,63 @@ const bot = new TelegramBot(token, { polling: true });
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
 
-  // O'yinni boshlash tugmasini yaratish
+  // 3 ta menyu tugmasini yaratish
   const menuOptions = {
     reply_markup: {
       keyboard: [
-        [{ text: 'O\'yinni boshlash' }]
+        [{ text: 'Menyu 1' }, { text: 'Game' }, { text: 'Menyu 3' }] // 3 ta menyu tugmasi
       ],
-      resize_keyboard: true, // Tugmani mos hajmga keltirish
-      one_time_keyboard: true // Bir martalik tugma
+      resize_keyboard: true // Tugmani mos hajmga keltirish
     }
   };
 
-  // Foydalanuvchiga o'yin tugmasini yuborish
-  bot.sendMessage(chatId, 'O\'yinni boshlash uchun tugmani bosing:', menuOptions);
+  // Foydalanuvchiga menyu tugmalarini yuborish
+  bot.sendMessage(chatId, 'Menyuni tanlang:', menuOptions);
 });
 
-// O'yinni boshlash tugmasini qayta ishlash
+// Menyu tugmalarini qayta ishlash
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
-  if (msg.text === 'O\'yinni boshlash') {
-    bot.sendMessage(chatId, 'Mana o\'yin sayti: https://000quvonchbek.github.io/multi.bot/');
-  } else {
-    bot.sendMessage(chatId, 'O\'yinni boshlash tugmasini tanlang.');
+  if (msg.text === 'Menyu 1') {
+    // O'yinni boshlash tugmasi - Menyu 1
+    const gameOptions1 = {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'O\'yinni boshlash', url: 'https://exemple.com' }]
+        ]
+      }
+    };
+
+    // Foydalanuvchiga o'yin tugmasini yuborish - Menyu 1
+    bot.sendMessage(chatId, 'Menyu 1 uchun o\'yinni boshlash tugmasini bosing:', gameOptions1);
+  }
+
+  if (msg.text === 'Game') {
+    // O'yinni boshlash tugmasi - Menyu 2
+    const gameOptions2 = {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'O\'yinni boshlash', url: 'https://000quvonchbek.github.io/multi.bot/' }]
+        ]
+      }
+    };
+
+    // Foydalanuvchiga o'yin tugmasini yuborish - Menyu 2
+    bot.sendMessage(chatId, 'Menyu 2 uchun o\'yinni boshlash tugmasini bosing:', gameOptions2);
+  }
+
+  if (msg.text === 'Menyu 3') {
+    // O'yinni boshlash tugmasi - Menyu 3
+    const gameOptions3 = {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'O\'yinni boshlash', url: 'https://example.com/menyu3' }]
+        ]
+      }
+    };
+
+    // Foydalanuvchiga o'yin tugmasini yuborish - Menyu 3
+    bot.sendMessage(chatId, 'Menyu 3 uchun o\'yinni boshlash tugmasini bosing:', gameOptions3);
   }
 });
